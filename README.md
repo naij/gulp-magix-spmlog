7b3c0ad57b5ada1afc1f68bc3d8c13988a53e7ab
陆议 <fruit.wj@taobao.com>
2015-03-19 10:17:15 +0800

    up

--- README.md
+++ README.md
@@ -18,9 +18,9 @@ gulp.task('spmlog', function () {
     gulp.src('./app/views/*/*.html')
         .pipe(spmlog({
             logkey: 'tblm.88.1',
-            sizzle: [
-                '[mx-click]',
-                '[href^="#!"]'
+            filter: [
+                'mx-click="[^\\"]+"',
+                'href="#![^\\"]+"'
             ]
         }))
         .pipe(gulp.dest('./app/views'));
@@ -33,6 +33,6 @@ gulp.task('spmlog', function () {
     
     其值为黄金令箭中的所属业务段（必填），业务段在令箭申请中心申请，用于令箭日志数据的订阅和分发，在后续扩展实时计算和展现的时候十分重要。
 
-- `sizzle`
+- `filter`
     
-    需要埋点的元素选择器（必填），只有满足相关选择器的元素才会打点。具体选择器请参考：[https://github.com/jquery/sizzle/wiki](https://github.com/jquery/sizzle/wiki)
+    需要埋点的元素过滤器（必填），只有满足相关过滤器的元素才会打点。支持正则。
--- index.js
+++ index.js
@@ -6,6 +6,14 @@ var through2  = require('through2');
 var sutil     = require('./lib/sutil');
 var Error     = gutil.PluginError;
 
+
+/**
+ * 给满足筛选条件的节点添加data-spm-click属性
+ * 对于已经生成过data-spm-click属性的节点，直接跳过
+ * 因为data-spm-click属性需要保持固定，只初始化生成一次
+ * 如果需要重新生成，可以把原来的属性删除掉
+ */ 
+
 function spmlog(options) {
     var logkey = options.logkey;
     var filter = options.filter;
@@ -24,8 +32,6 @@ function spmlog(options) {
         var filename = file.relative;
         var clickStr = 'gostr=/' + logkey + ';locaid=d';
 
-        // 如果已经生成过data-spm-click属性的节点，直接跳过
-        // 因为data-spm-click属性需要保持固定，只生成一次
         filter.forEach(function (item) {
             var matchExpr = new RegExp('(' + item + '(?!\\s*data-spm-click="[^\\"]+"))', 'g');
             html = html.replace(matchExpr, function (m, $1) {

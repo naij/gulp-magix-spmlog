fc5399e5577431b0eddc0105986f8852be7fa3a5
陆议 <fruit.wj@taobao.com>
2016-01-29 11:41:37 +0800

    修复locaid被删除的bug

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # gulp-magix-spmlog
 
-[![NPM version](https://img.shields.io/badge/npm-v1.0.4-orange.svg)](https://www.npmjs.org/package/gulp-magix-spmlog)
+[![NPM version](https://img.shields.io/badge/npm-v1.0.6-orange.svg)](https://www.npmjs.org/package/gulp-magix-spmlog)
 
 ## Installation
 
@@ -15,45 +15,45 @@ var gulp    = require('gulp');
 var spmlog  = require('gulp-magix-spmlog');
 
 gulp.task('spmlog', function () {
-    gulp.src('./app/views/*/*.html')
-        .pipe(spmlog({
-            logkey: 'mylogkey',
-            filter: [
-                '[mx-click]',
-                '[href^="#!"]'
-            ]
-        }))
-        .pipe(gulp.dest('./app/views'));
+  gulp.src('./app/views/*/*.html')
+    .pipe(spmlog({
+      logkey: 'mylogkey',
+      filter: [
+        '[mx-click]',
+        '[href^="#!"]'
+      ]
+    }))
+    .pipe(gulp.dest('./app/views'));
 });
 ```
 
 ## Options
 
 - `logkey`
-    
-    其值为黄金令箭中的所属业务段（必填），业务段在令箭申请中心申请，用于令箭日志数据的订阅和分发，在后续扩展实时计算和展现的时候十分重要。
+  
+  其值为黄金令箭中的所属业务段（必填），业务段在令箭申请中心申请，用于令箭日志数据的订阅和分发，在后续扩展实时计算和展现的时候十分重要。
 
-    *如果模板里面已经存在logkey，并且与当前要设置的logkey不相同，则会被替换为设置的logkey*
+  *如果模板里面已经存在logkey，并且与当前要设置的logkey不相同，则会被替换为设置的logkey*
 
 - `filter`
-    
-    需要埋点的元素过滤器（必填），只有满足相关过滤器的元素才会打点。
+  
+  需要埋点的元素过滤器（必填），只有满足相关过滤器的元素才会打点。
 
-    目前支持：
+  目前支持：
 
-    * id选择器
-    * class选择器
-    * attr选择器
+  * id选择器
+  * class选择器
+  * attr选择器
 
-    各个选择器格式：
+  各个选择器格式：
 
-    * id选择器： 
-        * '#main' 匹配`id`值为`main`的元素
-    * class选择器：
-        * '.content' 匹配`class`值为`content`的所有元素
-    * attr选择器：
-        * '[mx-click]' 匹配带有 `mx-click` 属性的所有元素
-        * '[href="#"]' 匹配带有 `href` 属性并且属性值为`#`的所有元素
-        * '[href^="#!"]' 匹配带有 `href` 属性并且属性值以`#!`开头的所有元素
-        * '[src$="jpg"]' 匹配带有 `href` 属性并且属性值以`jpg`结尾的所有元素
-        * '[custom*="value"]' 匹配带有 `custom` 属性并且属性值包含`value`的所有元素
+  * id选择器： 
+    * '#main' 匹配`id`值为`main`的元素
+  * class选择器：
+    * '.content' 匹配`class`值为`content`的所有元素
+  * attr选择器：
+    * '[mx-click]' 匹配带有 `mx-click` 属性的所有元素
+    * '[href="#"]' 匹配带有 `href` 属性并且属性值为`#`的所有元素
+    * '[href^="#!"]' 匹配带有 `href` 属性并且属性值以`#!`开头的所有元素
+    * '[src$="jpg"]' 匹配带有 `href` 属性并且属性值以`jpg`结尾的所有元素
+    * '[custom*="value"]' 匹配带有 `custom` 属性并且属性值包含`value`的所有元素
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var fs        = require('fs');
-var path      = require("path");
-var util      = require('util');
-var gutil     = require('gulp-util');
-var through2  = require('through2');
-var sutil     = require('./lib/sutil');
-var sizzle    = require('./lib/sizzle');
-var Error     = gutil.PluginError;
+var fs        = require('fs')
+var path      = require("path")
+var util      = require('util')
+var gutil     = require('gulp-util')
+var through2  = require('through2')
+var sutil     = require('./lib/sutil')
+var sizzle    = require('./lib/sizzle')
+var Error     = gutil.PluginError
 
 
 /**
@@ -16,57 +16,57 @@ var Error     = gutil.PluginError;
  */ 
 
 function spmlog(options) {
-    var logkey = options.logkey;
-    var filter = options.filter;
+  var logkey = options.logkey
+  var filter = options.filter
 
-    return through2.obj(function(file, enc, cb) {
+  return through2.obj(function(file, enc, cb) {
 
-        if (!logkey) {
-            return cb(new Error('spmlog', '缺少黄金令箭埋点串字段：logkey'));
+    if (!logkey) {
+      return cb(new Error('spmlog', '缺少黄金令箭埋点串字段：logkey'))
+    }
+
+    if (!filter || !util.isArray(filter)) {
+      return cb(new Error('spmlog', '缺少元素过滤规则字段：filter'))
+    }
+
+    var html = file.contents.toString('utf8')
+    var filename = file.relative
+    var clickStr = 'gostr=/' + logkey + ';locaid=d'
+    var rlogkey = new RegExp('(gostr=\\/([^\\";]+);)', 'g')
+
+    // 如果原来就有logkey并且与当前要设置的不相同，则替换成新的
+    html = html.replace(rlogkey, function (m, $1, $2) {
+      if ($2 && $2 != logkey) {
+        return 'gostr=/' + logkey + ';'
+      } else {
+        return $1
+      }
+    })
+
+    filter.forEach(function (item) {
+      var pattern = sizzle(item)
+
+      if (!pattern || !util.isArray(pattern)) {
+        return cb(new Error('spmlog', 'spmlog暂不支持此元素过滤器：' + item))
+      }
+      
+      var rmatch = new RegExp('(' + pattern[0] + '(?!\\s*data-spm-click="[^\\"]+"))', 'g')
+      var rvalue = new RegExp(pattern[1])
+      
+      html = html.replace(rmatch, function (m, $1, $2) {
+        if (rvalue.test($2)) {
+          var locaid = sutil.genRandom().substring(0, 8)
+          return $1 + ' ' + 'data-spm-click="' + clickStr + locaid + '"'
+        } else {
+          return $1
         }
+      })
+    })
 
-        if (!filter || !util.isArray(filter)) {
-            return cb(new Error('spmlog', '缺少元素过滤规则字段：filter'));
-        }
+    file.contents = new Buffer(html)
 
-        var html = file.contents.toString('utf8');
-        var filename = file.relative;
-        var clickStr = 'gostr=/' + logkey + ';locaid=d';
-        var rlogkey = new RegExp('(gostr=\\/([^\\"]+);)', 'g');
-
-        // 如果原来就有logkey并且与当前要设置的不相同，则替换成新的
-        html = html.replace(rlogkey, function (m, $1, $2) {
-            if ($2 && $2 != logkey) {
-                return 'gostr=/' + logkey + ';';
-            } else {
-                return $1;
-            }
-        });
-
-        filter.forEach(function (item) {
-            var pattern = sizzle(item);
-
-            if (!pattern || !util.isArray(pattern)) {
-                return cb(new Error('spmlog', 'spmlog暂不支持此元素过滤器：' + item));
-            }
-            
-            var rmatch = new RegExp('(' + pattern[0] + '(?!\\s*data-spm-click="[^\\"]+"))', 'g');
-            var rvalue = new RegExp(pattern[1]);
-            
-            html = html.replace(rmatch, function (m, $1, $2) {
-                if (rvalue.test($2)) {
-                    var locaid = sutil.genRandom().substring(0, 8);
-                    return $1 + ' ' + 'data-spm-click="' + clickStr + locaid + '"';
-                } else {
-                    return $1;
-                }
-            });
-        });
-
-        file.contents = new Buffer(html);
-
-        cb(null, file);
-    });
+    cb(null, file)
+  })
 }
 
-module.exports = spmlog;
+module.exports = spmlog

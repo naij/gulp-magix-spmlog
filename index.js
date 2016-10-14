d012e1bd63a64aea971da76c05dcea74d14e298c
yingchun.fyc <yingchun.fyc@alibaba-inc.com>
2016-10-14 20:29:27 +0800

    解决一个标签，同时满足选择条件，多次添加

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var Error     = gutil.PluginError
  * 对于已经生成过data-spm-click属性的节点，直接跳过
  * 因为data-spm-click属性需要保持固定，只初始化生成一次
  * 如果需要重新生成，可以把原来的属性删除掉
- */ 
+ */
 
 function spmlog(options) {
   var logkey = options.logkey
@@ -49,16 +49,21 @@ function spmlog(options) {
       if (!pattern || !util.isArray(pattern)) {
         return cb(new Error('spmlog', 'spmlog暂不支持此元素过滤器：' + item))
       }
-      
+
       var rmatch = new RegExp('(' + pattern[0] + '(?!\\s*data-spm-click="[^\\"]+"))', 'g')
+      var rtag = new RegExp('(<\\w+[\\s\\S]*?' + pattern[0] + '[\\s\\S]*?)/>|(<\\w+[\\s\\S]*?' + pattern[0] + '[\\s\\S]*?)>', 'g')
       var rvalue = new RegExp(pattern[1])
-      
-      html = html.replace(rmatch, function (m, $1, $2) {
-        if (rvalue.test($2)) {
-          var locaid = sutil.genRandom().substring(0, 8)
-          return $1 + ' ' + 'data-spm-click="' + clickStr + locaid + '"'
+
+      html = html.replace(rtag, function (m, $1, $2, $3, $4) {
+        var locaid = sutil.genRandom().substring(0, 8)
+        // 如果标签/结尾，判断有没有data-spm-click
+        if ($1 && $2 && !/data-spm-click="[^\\"]+/.test($1) && rvalue.test($2)) {
+          return $1 + ' ' + 'data-spm-click="' + clickStr + locaid + '"' + '/>'
+          // 如果标签不是/结尾，判断有没有data-spm-click
+        } else if($3 && $4 && !/data-spm-click="[^\\"]+/.test($3) && rvalue.test($4)) {
+          return $3 + ' ' + 'data-spm-click="' + clickStr + locaid + '"' + '>'
         } else {
-          return $1
+          return m
         }
       })
     })
